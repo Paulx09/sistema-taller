816b2d40e57d5c4d6ee12f77226d3408aa566699
Paulo Armey <pauloarmey18@gmail.com>
2026-02-16 21:14:39 -0500

    feat(client): initialize React project with Vite, TypeScript, and Tailwind CSS

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-green-500">
+      Sistema de Taller + Inventario
+    </h1>
+  )
+}
+
+export default App
